89652bf29fdc3cdd41dbe3b8801d708ca8bc381c
Jordan Durci <durcij@aldenv158.allegheny.edu>
2017-10-11 09:44:59 -0400

    Completed and debugged the code.

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -38,10 +38,13 @@ public class ManipulateDna {
       System.out.println("Unable to locate file");
     }
     String fullDna = "ACTG";
+    int fullLength = fullDna.length();
     // Step One: Read in and display the chosen DNA string
     String dna = scanner.nextLine();
+    String dnaUp = dna.toUpperCase();
+    System.out.println("Original DNA String:  " + dnaUp);
     // Step Two: Compute the complement of the DNA String
-    String dnaComp = dna.replace('A', 't');
+    String dnaComp = dnaUp.replace('A', 't');
     String dnaComp2 = dnaComp.replace('T', 'a');
     String dnaComp3 = dnaComp2.replace('G', 'c');
     String dnaComp4 = dnaComp3.replace('C', 'g');
@@ -49,23 +52,23 @@ public class ManipulateDna {
     System.out.println("Complement DNA String:  " + dnaCompFin);
     // Step Three: Insert a randomly chosen DNA letter into the DNA String
     Random rando = new Random();
-    String mut1 = "" + dnaCompFin.substring(1)
-        + dnaCompFin.charAt(rando.nextChar(fullDna.substring()))//error
+    String mut1 = "" + dnaCompFin.substring(0,2)
+        + fullDna.charAt(rando.nextInt(fullLength))
         + dnaCompFin.substring(2, dnaCompFin.length());
     System.out.println("Insertion mutation:  " + mut1);
     // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
     int loc1 = rando.nextInt(dnaCompFin.length()); // Picks a random location
-    String mut2 = "" + dnaCompFin.substring(0, loc1 - 1)
+    String mut2 = "" + dnaCompFin.substring(0, loc1)
         + dnaCompFin.substring(loc1 + 1, dnaCompFin.length());
     System.out.println("Random deletion mutation:  " + mut2);
     // Step Five: Change a random position in the DNA String to a randomly chosen DNA letter
     int loc2 = rando.nextInt(dnaCompFin.length()); // Picks a random location
-    String mut3 = "" + dnaCompFin.substring(0,loc2 - 1)
-        + dnaCompFin.charAt(rando.nextChar(fullDna.substring()))//error
-        + dnaCompFin.substring(loc2 + 1,dnaCompFin.length());
+    String mut3 = "" + dnaCompFin.substring(0,loc2)
+        + fullDna.charAt(rando.nextInt(fullLength))//error
+        + dnaCompFin.substring(loc2,dnaCompFin.length());
     System.out.println("Random insertion mutation:  " + mut3);
     // Step Six: Display a final thank you message
     System.out.println("Merci beaucoup, monsieur, mademoiselle, madame,"
         + " ou autre!");
-    }
+  }
 }
--- writing/reflection.md
+++ writing/reflection.md
@@ -1,4 +1,14 @@
-# Reflection by
+# Reflection by Jordan Durci
 
-This lab required me to write a file called `ManipulateDNA`. Please review the
-Laboratory Assignment Sheet and add the required details to this paragraph.
+This lab was somewhat difficult, but was still fairly straightforward.  The
+main difficulties came from working out how to effectively randomize locations
+and characters, which I solved by taking it one step at a time.  I did run into
+issues randomizing the character to be inserted, mainly because I was trying to
+randomize by selecting a character from the wrong string.  Once I fixed that,
+there was an issue with the substrings immediately before and after random
+characters, but that came from minor issues defining the bounds of the
+substrings.
+
+Aside from the issues above, it was fairly simple to work on, and figure out.
+It was pretty fun to change the letters from the input to prep the
+complementary DNA for all of the mutations.
